a0c964839b866b86bd62aa4aee848b7e38314b60
MrRokhan <khattak.rokhan@gmail.com>
2025-02-05 00:30:34 +1000

    js for age calculations

--- script.js
+++ script.js
@@ -5,9 +5,28 @@ const urAge = document.getElementById("ageResult");
 function calcAge() {
   const birthdayValue = dateOB.value;
   if (birthdayValue === "") {
-    alert("enter your dob");
+    alert("Enter your DOB");
   } else {
     const age = getAge(birthdayValue);
     urAge.innerText = `Your age is ${age} ${age > 1 ? "years" : "year"} old`;
   }
 }
+
+function getAge(birthdayValue) {
+  const currentDate = new Date();
+  const birthdayDate = new Date(birthdayValue);
+  let age = currentDate.getFullYear() - birthdayDate.getFullYear();
+  const month = currentDate.getMonth() - birthdayDate.getMonth();
+
+  if (
+    month < 0 ||
+    (month === 0 && currentDate.getDate() < birthdayDate.getDate())
+  ) {
+    age--;
+  }
+
+  return age;
+}
+
+// Correct event listener
+calcBtn.addEventListener("click", calcAge);
